9b4a47233c98ef1ff39073d95c254774daeea3f7
Norman Lopez <noell.lpz@gmail.com>
2024-03-28 18:30:35 -0700

    Add FizzBuzz implementation

--- hw4/FizzBuzz/src/main.rs
+++ hw4/FizzBuzz/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    fizz_buzz();
+}
+
+fn fizz_buzz() {
+    for n in 1..=301 {
+        if n % 3 == 0 && n % 5 == 0 {
+            println!("fizz buzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        }
+    }
 }
